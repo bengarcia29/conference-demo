c61b459991feb739d544a2085c938e9b395432bf
Naix <edvelez.g@gmail.com>
2020-05-17 21:01:58 -0500

    wip

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -1,6 +1,5 @@
 package com.pluralsight.conferencedemo.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,15 +8,12 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     @Bean
     public DataSource dataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(dbUrl);
-        System.out.println("Calling custom config for : " + dbUrl);
-        return dataSourceBuilder.build();
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        System.out.println("My custom datasource bean has been initialized and set");
+        return builder.build();
     }
+
 }
--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakerController.java
@@ -1,6 +1,5 @@
 package com.pluralsight.conferencedemo.controllers;
 
-import com.pluralsight.conferencedemo.models.Session;
 import com.pluralsight.conferencedemo.models.Speaker;
 import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
 import org.springframework.beans.BeanUtils;
@@ -33,5 +32,19 @@ public class SpeakerController {
         return speakerRepository.saveAndFlush(speaker);
     }
 
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        //Also need to check for children records before deleting.
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        //because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what has changed.
+        //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
 
 }
