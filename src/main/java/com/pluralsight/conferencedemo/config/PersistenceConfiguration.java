3190a4e44adff96482a9ec73f68838bc108e883a
Naix <edvelez.g@gmail.com>
2020-05-17 21:16:34 -0500

    for local

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,12 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
-    }
+    // only for local
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//        System.out.println("My custom datasource bean has been initialized and set");
+//        return builder.build();
+//    }
 
 }
